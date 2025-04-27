c04939c6662f432439449ae02ced131cdd6ecf8e
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-04-27 16:23:33 -0300

    Modifico código para administrar DOM e importar librerías de los demás scripts

---
+++ js/main.js
@@ -0,0 +1,32 @@
+import { uploadFileToS3 } from './s3Upload.js';
+import { iniciarTranscripcion } from './transcribe.js';
+
+const $formulario = document.getElementById('uploadForm');
+
+$formulario.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const fileInput = document.getElementById('audioFile');
+  const idiomaInput = document.getElementById('idioma');
+  const speakersInput = document.getElementById('speakers');
+
+  if (!fileInput.files.length) {
+    alert("Por favor seleccioná un archivo");
+    return;
+  }
+
+  const file = fileInput.files[0];
+  const key = `audios/${file.name}`;
+  const idioma = idiomaInput.value;
+  const speakers = parseInt(speakersInput.value);
+
+  try {
+    await uploadFileToS3(file, key);
+    console.log("Archivo subido correctamente");
+
+    const transcripcion = await iniciarTranscripcion(file.name, idioma, speakers);
+    console.log("Transcripción iniciada:", transcripcion);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
