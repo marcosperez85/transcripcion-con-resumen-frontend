4f7c64f91e46c7308743b3eb0b7e24f60a39a75d
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-04-27 20:49:03 -0300

    Actualizo código eliminando los import para usar las librerías que se importan en el index.html

--- js/cognitoAuth.js
+++ js/cognitoAuth.js
@@ -1,15 +1,9 @@
-import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
-import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
-
-// Parámetros
-const REGION = "us-east-2";
-const IDENTITY_POOL_ID = "us-east-2:d8965005-3395-406b-b4cf-9140dffbc1b0";
-
 export function getCognitoCredentials() {
-  return fromCognitoIdentityPool({
-    client: new CognitoIdentityClient({ region: REGION }),
-    identityPoolId: IDENTITY_POOL_ID
+  return AWS.fromCognitoIdentityPool({
+    client: new AWS.CognitoIdentityClient({ region: "us-east-2" }),
+    identityPoolId: "us-east-2:d8965005-3395-406b-b4cf-9140dffbc1b0"
   });
 }
+
 
 
