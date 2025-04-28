0e8ab49ed9c7cdb5b5fa2a881a88c87d41cbc18c
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-04-27 21:50:44 -0300

    Eliminé el AWS. dado que hago los llamados desde los SDK que instale

--- src/js/cognitoAuth.js
+++ src/js/cognitoAuth.js
@@ -2,8 +2,8 @@ import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-id
 import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 
 export function getCognitoCredentials() {
-  return AWS.fromCognitoIdentityPool({
-    client: new AWS.CognitoIdentityClient({ region: "us-east-2" }),
+  return fromCognitoIdentityPool({
+    client: new CognitoIdentityClient({ region: "us-east-2" }),
     identityPoolId: "us-east-2:d8965005-3395-406b-b4cf-9140dffbc1b0"
   });
 }
