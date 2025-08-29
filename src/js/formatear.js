cc7606fea6b9d54c9eca86462863177832a7d471
Marcos Perez <marcospereztech@proton.me>
2025-08-29 12:48:16 -0300

    fix: arreglo URL para que refleje el nuevo endpoint y la nueva region en us-east-1

--- src/js/formatear.js
+++ src/js/formatear.js
@@ -1,5 +1,5 @@
 export async function formatearTranscripcion(nombreDelBucket, nombreDelJob, fileKey) {
-    const apiUrl = "https://vq9cf0otga.execute-api.us-east-2.amazonaws.com/prod/transcribir";
+    const apiUrl = "https://pvpva3kjdl.execute-api.us-east-1.amazonaws.com/prod/transcribir";
 
     const body = {
         s3: {
