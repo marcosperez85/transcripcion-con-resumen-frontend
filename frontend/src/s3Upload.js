8b4135dc76b8f67838f27df7cca0ff969991ed53
Marcos Perez <marcospereztech@proton.me>
2025-09-07 09:37:19 -0300

    fix: modifico nombre del bucket destino en el backend al que tiene que llamar s3Upload

--- frontend/src/s3Upload.js
+++ frontend/src/s3Upload.js
@@ -3,7 +3,7 @@ import { Upload } from "@aws-sdk/lib-storage";
 import { getCognitoCredentials } from './cognitoAuth.js';
 
 const REGION = "us-east-1";
-const BUCKET_NAME = "transcripcion-con-resumen-backend";
+const BUCKET_NAME = "transcripcion-con-resumen-backend-376129873205-us-east-1";
 
 // Importante configurar el CORS en la pestaña de Permissions dentro del bucket para que admita el origen desde el cual 
 // se realiza el upload. De todas formas ese permiso ya fue agregado en el stack.
