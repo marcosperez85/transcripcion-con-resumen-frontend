5e7b0334eef29fb876c6bc6e7aeffcd624d47b35
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-09-09 17:30:49 -0300

    fix: agrego nueva ruta en vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        app: resolve(__dirname, 'pages/app.html')
+        app: resolve(__dirname, 'pages/app.html'),
+        callback: resolve(__dirname, 'pages/callback.html')
       }
     }
   },
