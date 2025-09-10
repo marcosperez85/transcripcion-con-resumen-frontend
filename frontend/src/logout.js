4cbe3da775b2afaac11f3ae3e91bfaa3437579f0
Marcos Perez <marcospereztech@proton.me>
2025-09-09 23:58:51 -0300

    fix: renombro archivos de login y logout para separar funciones

--- frontend/src/logout.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-import { signOutRedirect } from "./auth.js";
-
-document.getElementById("logoutBtn").addEventListener("click", async () => {
-    console.log("Logout Presionado")
-    await signOutRedirect();
-});
