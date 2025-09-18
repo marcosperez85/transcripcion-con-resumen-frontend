7b078b382762b22e5d64e25db5b7ec5a1d0f6e42
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-09-18 17:09:34 -0300

    feat: agrego archivo para centralizar la declaración de variables globales

---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+// Archivo exclusivo para configuración - NO importa nada más
+export const REGION = "us-east-1";
+export const USER_POOL_ID = "us-east-1_vQyS5mw64";
+export const USER_POOL_CLIENT_ID = "c8k2hoamcdgp44u89m16k0s0f";
+export const IDENTITY_POOL_ID = "us-east-1:d48bc7ec-5785-47f0-a6a4-4ca30d43b3a2";
+export const CLIENT_ID = "6evgd9kupcn26vc5nmtuajqrkm";
+export const COGNITO_DOMAIN = `https://${USER_POOL_ID}.auth.${REGION}.amazoncognito.com`;
+export const BUCKET_NAME = "transcripcion-con-resumen-backend-376129873205-us-east-1";
