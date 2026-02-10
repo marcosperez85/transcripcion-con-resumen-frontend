96c7c6830de5675250c1098da086c6bf573f2a36
Marcos Perez <perezmarcos04@yahoo.com.ar>
2026-02-10 17:42:36 -0300

    fix: Reemplaza valores hardcodeados por referencias a CONFIG

--- frontend/src/s3Credentials.js
+++ frontend/src/s3Credentials.js
@@ -1,47 +1,35 @@
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 import { CognitoIdentityClient, GetIdCommand } from "@aws-sdk/client-cognito-identity";
 import { userManager } from "./auth.js";
-
- // Ajusta región según corresponda
-const REGION = "us-east-1";
-
-// Es el mismo que está en "auth.js"
-const USER_POOL_ID = "us-east-1_cH9mKVza7"; 
-
-// Obtenido del output del CDK del backend
-const IDENTITY_POOL_ID = "us-east-1:d48bc7ec-5785-47f0-a6a4-4ca30d43b3a2";
-
-// us-east-1:57931665-35be-c0e7-67f5-d6da3dd59c4b
-
-const PROVIDER = `cognito-idp.${REGION}.amazonaws.com/${USER_POOL_ID}`;
+import { CONFIG } from "./config.js";
 
 export async function getCognitoCredentials() {
-  const user = await userManager.getUser();
-  if (!user || user.expired) {
-    throw new Error("Usuario no autenticado o sesión expirada.");
-  }
-  const idToken = user.id_token;
+    const user = await userManager.getUser();
+    if (!user || user.expired) {
+        throw new Error("Usuario no autenticado o sesión expirada.");
+    }
 
-  return fromCognitoIdentityPool({
-    client: new CognitoIdentityClient({ region: REGION }),
-    identityPoolId: IDENTITY_POOL_ID,
-    logins: { [PROVIDER]: idToken },
-  });
+    return fromCognitoIdentityPool({
+        client: new CognitoIdentityClient({ region: CONFIG.REGION }),
+        identityPoolId: CONFIG.IDENTITY_POOL_ID,
+        logins: {
+            [CONFIG.PROVIDER]: user.id_token
+        },
+    });
 }
 
-// (Opcional) Para construir el prefijo 'audios/<IdentityId>/*' en el key:
 export async function getIdentityId() {
-  const user = await userManager.getUser();
-  if (!user || user.expired) {
-    throw new Error("Usuario no autenticado o sesión expirada.");
-  }
+    const user = await userManager.getUser();
+    if (!user || user.expired) {
+        throw new Error("Usuario no autenticado o sesión expirada.");
+    }
 
-  const idToken = user.id_token;
-  const client = new CognitoIdentityClient({ region: REGION });
+    const client = new CognitoIdentityClient({ region: CONFIG.REGION });
 
-  const out = await client.send(new GetIdCommand({
-    IdentityPoolId: IDENTITY_POOL_ID,
-    Logins: { [PROVIDER]: idToken },
-  }));
-  return out.IdentityId; // ej: "us-east-1:1234abcd-...."
+    const out = await client.send(new GetIdCommand({
+        IdentityPoolId: CONFIG.IDENTITY_POOL_ID,
+        Logins: { [CONFIG.PROVIDER]: user.id_token },
+    }));
+
+    return out.IdentityId;
 }
