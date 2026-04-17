5491893b40357531f7dde04853a3c170b75b6828
Marcos Perez <marcospereztech@proton.me>
2026-04-17 17:57:43 -0300

    feat: implementa manejo de eventos para el botón de registro de usuarios

--- frontend/src/loginHandler.js
+++ frontend/src/loginHandler.js
@@ -1,9 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import { userManager } from "./auth.js";
+import { userManager, redirectToSignUp } from "./auth.js";
 
 document.getElementById("loginBtn").addEventListener("click", async () => {
     console.log("Login Presionado")    
     await userManager.signinRedirect();
 });
+
+// Añadir evento para el botón de registro
+const registerBtn = document.getElementById("registerBtn");
+if (registerBtn) {
+    registerBtn.addEventListener("click", () => {
+        console.log("Registro Presionado");
+        redirectToSignUp();
+    });
+}
